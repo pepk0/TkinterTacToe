ba8e59c6d658a191aa602d3514fd2e2307b6d96d
Петър Попов <popovstz@gmail.com>
2024-02-10 15:52:57 +0200

    Update window.py

--- gui/window.py
+++ gui/window.py
@@ -2,8 +2,20 @@ import tkinter as tk
 from tkinter import ttk
 
 
+class ButtonFrame(tk.Frame):
+    def __init__(self) -> None:
+        super().__init__()
+        self.new_button = ttk.Button(self, text="New Game", width=20)
+        self.quit_button = ttk.Button(self, text="Quit")
+
+        self.new_button.grid(row=0, column=0, padx=5)
+        self.quit_button.grid(row=0, column=1)
+
+
 class MainFrame(tk.Tk):
-    def __init__(self,) -> None:
+    def __init__(self, ) -> None:
         super().__init__()
         self.title("Tic Tac Toe")
         self.geometry("350x350")
+        self.resizable(False, False)
+        self.option_frame = ButtonFrame().grid(row=0, column=0)
