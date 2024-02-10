219c5593cf681d65da0f363453e582d4448f1924
Петър Попов <popovstz@gmail.com>
2024-02-10 23:37:56 +0200

    ref

--- gui/window.py
+++ gui/window.py
@@ -1,70 +1,40 @@
 import tkinter as tk
+
+from PIL import Image, ImageTk
 from tkinter import ttk
 from math import ceil
 
 
+class FrameButton(tk.Frame):
+    def __init__(self,) -> None:
+        super().__init__(width=80, height=80, bg="Gray")
+        image_one = ImageTk.PhotoImage(
+            Image.open(r"imgs\x.png").resize((80, 80)))
+        self.button = tk.Button(self, image=image_one, width=80, height=80,
+             command=lambda: on_press(self, image_one))
+
+        self.button.grid(row=0, column=0)
+
+        def on_press(self, image) -> None:
+            print("clicked")
+            self.button.image = image
+            self.button["image"] = image
+
+
 class MainFrame(tk.Tk):
-    def __init__(self, ) -> None:
+    def __init__(self,) -> None:
         super().__init__()
         self.title("Tic Tac Toe")
         self.resizable(False, False)
         self.turn = 0
         self.matrix_field = [[0] * 3 for _ in range(3)]
 
-        self.position_one = tk.Button(
-            self, text="", width=12, height=5, bg="Grey", relief="flat",
-            activebackground="Gray", border=0,
-            command=lambda: place_spot(self, 1, self.position_one))
-
-        self.position_two = tk.Button(
-            self, text="", width=12, height=5, bg="Grey", relief="flat",
-            activebackground="Gray", border=0,
-            command=lambda: place_spot(self, 2, self.position_two))
-
-        self.position_three = tk.Button(
-            self, text="", width=12, height=5, bg="Gray", relief="flat",
-            activebackground="Gray", border=0,
-            command=lambda: place_spot(self, 3, self.position_three))
-
-        self.position_four = tk.Button(
-            self, text="", width=12, height=5, bg="Grey", relief="flat",
-            activebackground="Gray", border=0,
-            command=lambda: place_spot(self, 4, self.position_four))
-
-        self.position_five = tk.Button(
-            self, text="", width=12, height=5, bg="Grey", relief="flat",
-            activebackground="Gray", border=0,
-            command=lambda: place_spot(self, 5, self.position_five))
-
-        self.position_six = tk.Button(
-            self, text="", width=12, height=5, bg="Grey", relief="flat",
-            activebackground="Gray", border=0,
-            command=lambda: place_spot(self, 6, self.position_six))
-
-        self.position_seven = tk.Button(
-            self, text="", width=12, height=5, bg="Grey", relief="flat",
-            activebackground="Gray", border=0,
-            command=lambda: place_spot(self, 7, self.position_seven))
-
-        self.position_eight = tk.Button(
-            self, text="", width=12, height=5, bg="Grey", relief="flat",
-            activebackground="Gray", border=0,
-            command=lambda: place_spot(self, 8, self.position_eight))
-
-        self.position_nine = tk.Button(
-            self, text="", width=12, height=5, bg="Grey", relief="flat",
-            activebackground="Gray", border=0,
-            command=lambda: place_spot(self, 9, self.position_nine))
-
-        self.position_one.grid(row=0, column=0, padx=2, pady=2)
-        self.position_two.grid(row=0, column=1, padx=2, pady=2)
-        self.position_three.grid(row=0, column=2, padx=2, pady=2)
-        self.position_four.grid(row=1, column=0, padx=2, pady=2)
-        self.position_five.grid(row=1, column=1, padx=2, pady=2)
-        self.position_six.grid(row=1, column=2, padx=2, pady=2)
-        self.position_seven.grid(row=2, column=0, padx=2, pady=2)
-        self.position_eight.grid(row=2, column=1, padx=2, pady=2)
-        self.position_nine.grid(row=2, column=2, padx=2, pady=2)
-
-        def place_spot(self, position: int, widget) -> None:
-            pass
+        self.button_one = FrameButton().grid(row=0, column=0, padx=2, pady=2)
+        self.button_two = FrameButton().grid(row=0, column=1, padx=2, pady=2)
+        self.button_tree = FrameButton().grid(row=0, column=2, padx=2, pady=2)
+        self.button_four = FrameButton().grid(row=1, column=0, padx=2, pady=2)
+        self.button_five = FrameButton().grid(row=1, column=1, padx=2, pady=2)
+        self.button_six = FrameButton().grid(row=1, column=2, padx=2, pady=2)
+        self.button_seven = FrameButton().grid(row=2, column=0, padx=2, pady=2)
+        self.button_eight = FrameButton().grid(row=2, column=1, padx=2, pady=2)
+        self.button_nine = FrameButton().grid(row=2, column=2, padx=2, pady=2)
