9a37e5b64a8c7b367e29d6af68c090f39fb61d33
Петър Попов <popovstz@gmail.com>
2024-02-11 18:36:58 +0200

    Update window.py

--- gui/window.py
+++ gui/window.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import ttk
 from math import ceil
+from tkinter import messagebox
 
 
 class FrameButton(tk.Frame):
@@ -23,8 +24,7 @@ class FrameButton(tk.Frame):
         self.button.grid(row=0, column=0)
 
         def on_press(self) -> None:
-            row = ceil(position / 3) - 1
-            col = (position + 2) % 3
+            row, col = ceil(position / 3) - 1, (position + 2) % 3
 
             if matrix[row][col] == 0:
                 element = "O" if main.turn % 2 == 0 else "X"
@@ -34,6 +34,12 @@ class FrameButton(tk.Frame):
                 self.button.image = img
                 self.button["image"] = img
                 main.turn += 1
+
+
+class InfoFrame(tk.Frame):
+    def __init__(self,) -> None:
+        super().__init__()
+        pass
 
 
 class MainFrame(tk.Tk):
@@ -65,11 +71,43 @@ class MainFrame(tk.Tk):
         self.button_nine.grid(row=2, column=2, padx=2, pady=2)
 
         def check_for_winner(event, arg: int) -> None:
-            row = ceil(arg / 3) - 1
-            col = (arg + 2) % 3
-            if self.matrix_field[row][col] == 0:
-                pass
+            row, col = ceil(arg / 3) - 1, (arg + 2) % 3
+            
+            if self.matrix_field[row][col] != 0:
+                return
+            
+            element = "O" if self.turn % 2 == 0 else "X"
+            self.matrix_field[row][col] = element  # type: ignore
 
+            directions = [(0, 1), (1, 0), (1, 1), (-1, 1)]
+            cur_row, cur_col = row, col
+
+            while directions:
+
+                next_row, next_col = directions.pop()
+                connected = 1
+
+                for i in range((3 * 2) - 2):
+
+                    if i == 3 - 1:
+                        cur_row, cur_col = row, col
+                        next_row, next_col = next_row * -1, next_col * -1
+
+                    cur_row, cur_col = next_row + cur_row, next_col + cur_col
+
+                    if 0 <= cur_row < 3 and 0 <= cur_col < 3:
+                        if self.matrix_field[cur_row][cur_col] == element:
+                            connected += 1
+
+                    if connected >= 3:
+                        self.matrix_field[row][col] = 0
+                        messagebox.showinfo(
+                            "Game Finished", f"Won => {element}")
+                        raise SystemExit
+
+                cur_row, cur_col = row, col
+
+            self.matrix_field[row][col] = 0
 
         self.button_one.button.bind(
             "<Button-1>", lambda event, arg=1: check_for_winner(event, arg))
@@ -89,3 +127,4 @@ class MainFrame(tk.Tk):
             "<Button-1>", lambda event, arg=8: check_for_winner(event, arg))
         self.button_nine.button.bind(
             "<Button-1>", lambda event, arg=9: check_for_winner(event, arg))
+
