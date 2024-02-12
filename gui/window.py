edbdef584662f1c25a62d80bd64ac327f40248f9
Петър Попов <popovstz@gmail.com>
2024-02-12 21:01:14 +0200

    added some comments and refactored some code

--- gui/window.py
+++ gui/window.py
@@ -54,24 +54,30 @@ class MainFrame(tk.Tk):
         self.resizable(False, False)
         self.turn = 0
         self.matrix_field = [[0] * 3 for _ in range(3)]
+        # mapping so we can use it to convert 2d matrix coordinates,
+        # to potions in range 1 - 9
         self.button_mapping = {}
 
         self.info_field = InfoFrame()
-        self.info_field.grid(row=0, column=0)
 
+        # frame placement
         for i in range(1, 10):
             row, col = get_coordinates(i)
             grid_pos = GridPosition(i)
             self.button_mapping[(row, col)] = grid_pos
             grid_pos.grid(row=row + 1, column=col, padx=2, pady=2,)
+            # on click event binding instead of a button command
             grid_pos.button.bind("<Button-1>", lambda event,
-                                 frame=grid_pos: check_for_winner(event, frame))
+                                 frame=grid_pos: on_button_click(event, frame))
 
-        def check_for_winner(event, frame) -> None:
+        self.info_field.grid(row=0, column=0)
+
+        def on_button_click(event, frame) -> None:
             position = frame.number
             element = "O" if self.turn % 2 == 0 else "X"
 
             if place_on_matrix(self.matrix_field, element, position):
+                # place a appropriate image on the button and info panel
                 frame.on_place(self)
                 self.info_field.set_img(self)
 
@@ -83,6 +89,8 @@ class MainFrame(tk.Tk):
                         "Game Finished", f"The winner is: '{element}' !")
                     raise SystemExit
 
+            # a maximum of 3x3 = 9 moves can be made,
+            # if no winner then its a draw
             if self.turn == 9:
                 messagebox.showinfo(
                     "Game Finished", f"The game is Draw!")
