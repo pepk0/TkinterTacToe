d415cd6dc5898cef8fe5793cd4189342484cf0d4
Петър Попов <popovstz@gmail.com>
2024-02-12 22:35:04 +0200

    removed pop up window set text to info label

--- gui/window.py
+++ gui/window.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 
 from PIL import Image, ImageTk
-from tkinter import messagebox
 from utils.functionality import (
     get_coordinates, place_on_matrix, get_winner, mark_winner)
 
@@ -31,19 +30,28 @@ class GridPosition(tk.Frame):
 class InfoFrame(tk.Frame):
     def __init__(self) -> None:
         super().__init__()
+        self.image_draw = self.image_O = ImageTk.PhotoImage(
+            Image.open(r"imgs\1x1.png").resize((1, 1)))
         self.image_O = ImageTk.PhotoImage(
             Image.open(r"imgs\o.png").resize((40, 40)))
         self.image_X = ImageTk.PhotoImage(
             Image.open(r"imgs\x.png").resize((40, 40)))
 
-        self.label = tk.Label(self, text=" is placing", font=("Helvetica", 25))
+        self.text_label = tk.Label(
+            self, text="Placing: ", font=("Helvetica", 25))
         self.img_label = tk.Label(self, image=self.image_O, height=90)
 
-        self.img_label.grid(row=0, column=0)
-        self.label.grid(row=0, column=1)
+        self.img_label.grid(row=0, column=1)
+        self.text_label.grid(row=0, column=0)
 
-    def set_img(self, parent):
+    def set_img(self, parent, element=None, draw=False):
         img = self.image_O if parent.turn % 2 == 0 else self.image_X
+
+        if draw:
+            img = self.image_draw
+        elif element:
+            img = self.image_O if element == "O" else self.image_X
+
         self.img_label["image"] = img
 
 
@@ -104,14 +112,14 @@ class MainFrame(tk.Tk):
 
                 if winning_pos:
                     mark_winner(winning_pos, self.button_mapping)
-                    messagebox.showinfo(
-                        "Game Finished", f"The winner is: '{element}' !")
+                    self.info_field.set_img(self, element=element)
+                    self.info_field.text_label["text"] = "Winner is: "
                     self.matrix_field = [["X"] * 3 for _ in range(3)]
                 # a maximum of 3x3 = 9 moves can be made,
                 # if no winner then it's a draw
                 elif self.turn == 9:
-                    messagebox.showinfo(
-                        "Game Finished", f"The game is Draw!")
+                    self.info_field.text_label["text"] = "The Game is Draw"
+                    self.info_field.set_img(self, draw=True)
 
         def quit_game(event) -> None:
             raise SystemExit
