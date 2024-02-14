42c790acb83994494f51fa22dbb3b99f3c69dd35
peter <popovstz@gmail.com>
2024-02-14 19:57:36 +0200

    added comments

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from gui.window import MainFrame
 
 
 def main() -> None:
-
+    # this loops makes the game run indefinitely
+    # until the quit game button is chosen
     while True:
         root = MainFrame()
         root.mainloop()
